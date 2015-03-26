08ddb3a14bc3d39bae47306922d4cf99200358ac
Stefan Steinhart <stefanreppner@gmail.com>
2015-03-26 10:10:16 +0100

    + making use of angular util functions + shimming angular.merge support + separate data and options for preparing real time update support

--- src/scripts/controllers/example.js
+++ src/scripts/controllers/example.js
@@ -6,99 +6,106 @@
  * Example controller.
  */
 angular
-  .module('example')
-  .controller('ExampleCtrl', ['$scope', '$http', '$log', function($scope, $http, $log) {
+    .module('example')
+    .controller('ExampleCtrl', ['$scope', '$http', '$log', function ($scope, $http, $log) {
 
-    $scope.STATES = {
-      READY: 0,
-      LOADING: 1,
-      ERROR: 2
-    };
+        $scope.STATES = {
+            READY: 0,
+            LOADING: 1,
+            ERROR: 2
+        };
 
-    $scope.alert = null;
-    $scope.charts = {
-      fakeUsers: {
-        data: null,
-        description: 'New user accounts by date',
-        src: 'data/fake_users1.json',
-        title: 'User Accounts',
-        x_accessor: 'date',
-        y_accessor: 'value'
-      },
-      fakeUsers2: {
-        data: null,
-        description: 'Some other user account information, by date',
-        src: 'data/fake_users2.json',
-        title: 'Some Other Account Information',
-        x_accessor: 'date',
-        y_accessor: 'value'
-      },
-      smallRanges: {
-        data: null,
-        description: 'Small value range',
-        src: 'data/small-range.json',
-        title: 'Small Value Range',
-        x_accessor: 'date',
-        y_accessor: 'value'
-      },
-      ufoSightings: {
-        data: null,
-        description: 'Unidentified flying object sightings in Australia by date',
-        src: 'data/ufo_sightings.json',
-        title: 'UFO Sightings',
-        x_accessor: 'date',
-        y_accessor: 'value'
-      }
-    };
-    $scope.state = $scope.STATES.READY;
+        $scope.alert = null;
+        $scope.charts = {
+            fakeUsers: {
+                data: null,
+                src: 'data/fake_users1.json',
+                options: {
+                    title: 'User Accounts',
+                    x_accessor: 'date',
+                    y_accessor: 'value'
+                }
+            },
+            fakeUsers2: {
+                data: null,
+                src: 'data/fake_users2.json',
+                options: {
+                    description: 'Some other user account information, by date',
+                    title: 'Some Other Account Information',
+                    x_accessor: 'date',
+                    y_accessor: 'value'
+                }
+            },
+            smallRanges: {
+                data: null,
+                src: 'data/small-range.json',
+                options: {
+                    description: 'Small value range',
+                    title: 'Small Value Range',
+                    x_accessor: 'date',
+                    y_accessor: 'value'
+                }
+            },
+            ufoSightings: {
+                data: null,
+                src: 'data/ufo_sightings.json',
+                options: {
+                    description: 'Unidentified flying object sightings in Australia by date',
+                    title: 'UFO Sightings',
+                    x_accessor: 'date',
+                    y_accessor: 'value'
+                }
+            }
+        };
+        $scope.state = $scope.STATES.READY;
 
-    //-------------------------------------------------------------------------
+        //-------------------------------------------------------------------------
 
-    /**
-     * Fix date representation.
-     * @param data Data collection
-     */
-    $scope.fix = function (data) {
-      if (Array.isArray(data) && Array.isArray(data[0])) {
-        data.forEach(function(element) {
-          $scope.fix(element);
-        });
-      } else {
-        MG.convert.date(data, 'date');
-      }
-    };
+        /**
+         * Fix date representation.
+         * @param data Data collection
+         */
+        $scope.fix = function (data) {
+            if (Array.isArray(data) && Array.isArray(data[0])) {
+                data.forEach(function (element) {
+                    $scope.fix(element);
+                });
+            } else {
+                MG.convert.date(data, 'date');
+            }
+        };
 
-    $scope.init = function () {
-      Object.keys($scope.charts).forEach(function(chart) {
-        $scope.read(chart);
-      });
-    };
+        $scope.init = function () {
+            Object.keys($scope.charts).forEach(function (chart) {
+                $scope.read(chart);
+            });
+        };
 
-    $scope.read = function (chart) {
-      $scope.alert = null;
-      $scope.state = $scope.STATES.LOADING;
-      if ($scope.charts[chart]) {
-        $http
-          .get($scope.charts[chart].src)
-          .success(function(data) {
-            $scope.charts[chart].data = data;
-            $scope.fix($scope.charts[chart].data);
-            $scope.state = $scope.STATES.READY;
-          })
-          .error(function(err) {
-            $scope.alert = {
-              type: 'ERROR',
-              message: err
-            };
-            $scope.charts[chart].data = null;
-            $scope.state = $scope.STATES.READY;
-            $log.error(err);
-          });
-      }
-    };
+        $scope.read = function (chart) {
+            $scope.alert = null;
+            $scope.state = $scope.STATES.LOADING;
+            if ($scope.charts[chart]) {
+                $http
+                    .get($scope.charts[chart].src)
+                    .success(function (data) {
+                        $scope.charts[chart].data = data;
+                        $scope.fix($scope.charts[chart].data);
+                        $scope.state = $scope.STATES.READY;
+                    })
+                    .error(function (err) {
+                        $scope.alert = {
+                            type: 'ERROR',
+                            message: err
+                        };
+                        $scope.charts[chart].data = null;
+                        $scope.state = $scope.STATES.READY;
+                        $log.error(err);
+                    });
+            }
+        };
 
-    // initialize the controller
-    $scope.init();
+        // initialize the controller
+        $scope.init();
 
-  }
-]);
+    }
+    ]);
--- src/scripts/directives/chart.js
+++ src/scripts/directives/chart.js
@@ -2,6 +2,78 @@
 /* jshint camelcase:false, unused:false */
 'use strict';
 
+if (!angular.merge) {
+
+    // shimming merge support since it available with ng1.4 but not below
+    // straight copy from angular 1.4.0-beta.6 sources
+
+    var slice=[].slice;
+
+    /**
+     * Set or clear the hashkey for an object.
+     * @param obj object
+     * @param h the hashkey (!truthy to delete the hashkey)
+     */
+    var setHashKey = function (obj, h) {
+        if (h) {
+            obj.$$hashKey = h;
+        } else {
+            delete obj.$$hashKey;
+        }
+    };
+
+
+    var baseExtend = function (dst, objs, deep) {
+        var h = dst.$$hashKey;
+
+        for (var i = 0, ii = objs.length; i < ii; ++i) {
+            var obj = objs[i];
+            if (!angular.isObject(obj) && !angular.isFunction(obj)) {
+                continue;
+            }
+            var keys = Object.keys(obj);
+            for (var j = 0, jj = keys.length; j < jj; j++) {
+                var key = keys[j];
+                var src = obj[key];
+
+                if (deep && angular.isObject(src)) {
+                    if (!angular.isObject(dst[key])) {
+                        dst[key] = angular.isArray(src) ? [] : {};
+                    }
+                    baseExtend(dst[key], [src], true);
+                } else {
+                    dst[key] = src;
+                }
+            }
+        }
+
+        setHashKey(dst, h);
+        return dst;
+    };
+
+    /**
+     * @ngdoc function
+     * @name angular.merge
+     * @module ng
+     * @kind function
+     *
+     * @description
+     * Deeply extends the destination object `dst` by copying own enumerable properties from the `src` object(s)
+     * to `dst`. You can specify multiple `src` objects. If you want to preserve original objects, you can do so
+     * by passing an empty object as the target: `var object = angular.merge({}, object1, object2)`.
+     *
+     * Unlike {@link angular.extend extend()}, `merge()` recursively descends into object properties of source
+     * objects, performing a deep copy.
+     *
+     * @param {Object} dst Destination object.
+     * @param {...Object} src Source object(s).
+     * @returns {Object} Reference to `dst`.
+     */
+    angular.merge = function merge(dst) {
+        return baseExtend(dst, slice.call(arguments, 1), true);
+    };
+}
+
 /**
  * MetricsGraphics
  * This directive is based on tassekatt's Stackoverflow post
@@ -14,37 +86,44 @@ angular.module('metricsgraphics', []).directive('chart', function () {
     return {
         link: function (scope, element) {
 
-            /**
-             * override or extend options with newly provided data
-             * @param newOptions
-             * @param oldOptions
-             */
-            function extendOptions(newOptions, oldOptions) {
-
-                // override previous options with new ones, if provided in new options
-                if (newOptions) {
-
-                    Object.keys(newOptions).forEach(function (key) {
-                        oldOptions[key] = newOptions[key];
-                    });
+            // create a random identifier for the chart element
+            // TODO replace this with a template that has a unique id
+            function randomString(len) {
+                var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                var s = '';
+                for (var i = 0; i < len; i++) {
+                    var randomPoz = Math.floor(Math.random() * charSet.length);
+                    s += charSet.substring(randomPoz, randomPoz + 1);
                 }
+                return 'mg-chart-' + s;
             }
 
             /**
              * force chart to redraw with given options
              *
+             * @param data
              * @param options
              */
-            function redraw(options) {
+            function redraw(data, options) {
 
+                //TODO do we have to provide some way of showing that there is no data? use what the library can show as "missing graph"?
                 // only draw if there is actually something to draw
-                if (options.data.length > 0) {
+                if (data && data.length > 0) {
 
-                    // !!! hack to force metricsgraphics to reevaluate time x values TODO: better solution?
-                    options.xax_format = null;
+                    // set data to nothing since we split data and options handling in this directive
+                    if (options.data) {
+                        options.data = null;
+                    }
+
+                    // create a copy of the original options to avoid reflecting
+                    // changes that are made to the options from the library
+                    var copy = angular.copy(options);
+
+                    // set the data
+                    copy.data = data;
 
                     // redraw chart
-                    MG.data_graphic(options);
+                    MG.data_graphic(copy);
                 }
             }
 
@@ -59,44 +138,45 @@ angular.module('metricsgraphics', []).directive('chart', function () {
                 x_accessor: null,
                 y_accessor: null
             };
+            // apply options from scope
+            angular.merge(options, scope.options);
 
-            extendOptions(scope.options, options);
-
-            // create a random identifier for the chart element
-            // TODO replace this with a template that has a unique id
-            function randomString(len) {
-                var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-                var s = '';
-                for (var i = 0; i < len; i++) {
-                    var randomPoz = Math.floor(Math.random() * charSet.length);
-                    s += charSet.substring(randomPoz, randomPoz + 1);
-                }
-                return 'mg-chart-' + s;
-            }
-
+            // set the elements id if not already set
             element[0].id = element[0].id ? element[0].id : randomString(5);
-            // set the data and target configuration options
-            options.data = scope.data || [];
+            // set the target id in the options
             options.target = '#' + element[0].id;
+
+            // set data from scope or default
+            var data = scope.data || [];
+
+            // initial draw
+            redraw(data, options);
 
             /**
              *  react to data changes
              */
-            scope.$watch('data', function (newValue, oldValue) {
+            scope.$watchCollection('data', function (newValue) {
 
-                options.data = newValue;
-                redraw(options);
-
-            }, false);
+                redraw(newValue, options);
+            });
 
             /**
              * react to option changes
              */
-            scope.$watch('options', function (newValue, oldValue) {
+            scope.$watch('options', function (newValue) {
 
-                extendOptions(newValue, options);
-                redraw(options);
+                // data has its own attribute binding
+                if (newValue.data) {
+                    newValue.data = null;
+                }
 
+                // resetting the target is not allowed
+                if (newValue.target) {
+                    delete newValue.target;
+                }
+
+                angular.merge(options, newValue);
+                redraw(data, newValue);
             }, true);
         },
         restrict: 'E',
